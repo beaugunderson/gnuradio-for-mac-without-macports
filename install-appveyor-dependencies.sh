a93bdc30e5a4d185075ddaac75e86957d5f1e141
Beau Gunderson <beau@beaugunderson.com>
2020-03-27 13:52:57 -0700

    add missing target

--- install-appveyor-dependencies.sh
+++ install-appveyor-dependencies.sh
@@ -7,4 +7,4 @@ PYTHON_VERSION="python-3.7.7-macosx10.9.pkg"
 
 wget "https://www.python.org/ftp/python/3.7.7/$PYTHON_VERSION"
 
-sudo installer -pkg "$PYTHON_VERSION" -target
+sudo installer -pkg "$PYTHON_VERSION" -target /
