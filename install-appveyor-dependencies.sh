18fa2c9f96aeb9d4f8391bd265300ad368c6be80
Beau Gunderson <beau@beaugunderson.com>
2020-03-27 13:48:02 -0700

    install Python 3.7

--- install-appveyor-dependencies.sh
+++ install-appveyor-dependencies.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 brew cask install xquartz
+brew install wget
+
+PYTHON_VERSION="python-3.7.7-macosx10.9.pkg"
+
+wget "https://www.python.org/ftp/python/3.7.7/$PYTHON_VERSION"
+
+sudo installer -pkg "$PYTHON_VERSION" -target
