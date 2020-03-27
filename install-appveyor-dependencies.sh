c30620029264d20f4f8b75b7f0254bb02e44b280
Beau Gunderson <beau@beaugunderson.com>
2020-03-27 13:38:07 -0700

    use brew

--- install-appveyor-dependencies.sh
+++ install-appveyor-dependencies.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-set -x
-
-XQUARTZ_VERSION=2.7.11
-
-wget "https://dl.bintray.com/xquartz/downloads/XQuartz-$XQUARTZ_VERSION.dmg"
-
-hdiutil attach "XQuartz-$XQUARTZ_VERSION.dmg"
-
-sudo installer -pkg "/Volumes/XQuartz-$XQUARTZ_VERSION/XQuartz.pkg" -target /
+brew cask install xquartz
